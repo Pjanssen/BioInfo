27b44adb9d6f8a7209cc00eb739d617c6b5ff0b0
Pier Janssen <pier@pjanssen.nl>
2013-10-27 23:19:06 +0100

    Added type synonyms, isReverseComplement function, comments

--- DNA.hs
+++ DNA.hs
@@ -1,20 +1,36 @@
 module BioInfo.DNA (
-     complement
+     Nucleotide
+   , Genome
+   , complement
    , reverseComplement
+   , isReverseComplement
 )
 where
 
 -------------------------------------------------------------------------------
 
-complement :: Char -> Char
+-- |A type synonym for a single nucleotide.
+type Nucleotide = Char
+
+-- |A type synonym for a genome, i.e. a list of nucleotides.
+type Genome = [Nucleotide]
+
+-------------------------------------------------------------------------------
+
+complement :: Nucleotide -> Nucleotide
 complement 'A' = 'T'
 complement 'T' = 'A'
 complement 'G' = 'C'
 complement 'C' = 'G'
-complement n   = error ("Unknown nucleotide " ++ [n])
+complement n   = error (n : " is an unknown nucleotide")
 
-reverseComplement :: String -> String
+-- |Creates the reverse complement of the given genome.
+reverseComplement :: Genome -> Genome
 reverseComplement genome = map complement $ reverse genome
+
+-- |Tests if the given genomes are reverse complements of eachother.
+isReverseComplement :: Genome -> Genome -> Bool
+isReverseComplement ns ns' = reverseComplement ns == ns'
 
 -------------------------------------------------------------------------------
 
