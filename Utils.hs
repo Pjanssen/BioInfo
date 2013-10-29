44e7eed35ae844ec9757dfe2a4d87f0ffc7023db
Pier Janssen <pier@pjanssen.nl>
2013-10-29 19:58:56 +0100

    Added isPartialMatch function, refactored subsetIndices

--- Utils.hs
+++ Utils.hs
@@ -1,12 +1,44 @@
 module BioInfo.Utils
 (
-   subsetIndices
+   -- * List functions
+     subsetIndices
+   , isPartialMatch
 )
 where
 
--- |Gets the starting indices where the given pattern matches a subset of the given list.
-subsetIndices :: (Eq x) => [x] -> [x] -> Int -> [Int]
-subsetIndices [] _ _                     = []
-subsetIndices xs p i | p `isPrefixOf` xs = i : rest
-                     | otherwise         = rest
-   where rest = subsetIndices (drop 1 xs) p (i + 1)
+import Data.List
+
+-------------------------------------------------------------------------------
+
+-- | Gets the starting indices where the given pattern matches a subset of the given list.
+--   For example
+--
+-- >>> subsetIndices "AG" "AAGTCAG"
+-- [1,5]
+subsetIndices :: (Eq x) => [x] -> [x] -> [Int]
+subsetIndices = subsetIndices' 0
+
+subsetIndices' :: (Eq x) => Int -> [x] -> [x] -> [Int]
+subsetIndices' _ _ []                      = []
+subsetIndices' i p xs | p `isPrefixOf` xs = i : rest
+                      | otherwise         = rest
+   where rest = subsetIndices' (i + 1) p (drop 1 xs)
+
+-------------------------------------------------------------------------------
+
+-- | Tests if the given lists are equal, with a given maximum number of mismatches.
+--   Examples: 
+--
+-- >>> isPartialMatch 1 "ACT" "ACA"
+-- True
+-- >>> isPartialMatch 1 "ACT" "ATA"
+-- False
+isPartialMatch :: (Eq x) => Int -> [x] -> [x] -> Bool
+isPartialMatch (-1) _ _ = False
+isPartialMatch _ [] []  = True
+isPartialMatch _ [] _   = False
+isPartialMatch _  _ []  = False
+isPartialMatch m (x:xs) (y:ys) | x == y    = isPartialMatch m xs ys
+                               | otherwise = isPartialMatch (m - 1) xs ys
+
+-------------------------------------------------------------------------------
